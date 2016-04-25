c6dd5e4fb369dd572eb0f8db740d829fd508a4a8
Daniel Vu <danielvu@Daniels-MacBook-Pro-2.local>
2016-04-25 12:56:53 -0700

    Completed beesbeesbees sprint.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
+  this.makeHoney = fuction() {
+    this.honeyPot++;
+  };
+  this.giveHoney = fuction() {
+    this.honeyPot--;
+  }
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
